cd55679a4af40cd122481225f0970851837038a3
BeLieVer031 <sandyrajak031@gmail.com>
2024-01-02 17:11:58 +0530

    genre song page navigation done

--- user-frontend/pages/home/home.js
+++ user-frontend/pages/home/home.js
@@ -9,8 +9,36 @@
 })();
 
 
-import { BUCKET_ID, DATABASE_ID, SONG_COLLECTION_ID } from "../../utils/sceret.js"
+import { BUCKET_ID, DATABASE_ID, GENRE_COLLECTION_ID, SONG_COLLECTION_ID } from "../../utils/sceret.js"
 import { databases, Query, storage } from "../../appwrite/config.js"
+
+const historyBox = document.querySelector(".history-box")
+
+const populateGenre = async () => {
+    try {
+        const genreArr = await databases.listDocuments(DATABASE_ID, GENRE_COLLECTION_ID,
+            [
+                Query.limit(4)
+            ])
+
+        genreArr.documents.forEach((element, index) => {
+            const historyCard = document.createElement("a")
+            historyCard.classList.add("history-card")
+            historyCard.classList.add("reset-anchor")
+            historyCard.href = `../genre-songs/genre-song.html?genre_name=${element.name}`
+            const html = `<div class="song-info">
+                            <div class="genre-name">${element.name.toUpperCase()}</div>
+                            <div class="song-name">classical</div>
+                        </div>`;
+            historyCard.innerHTML = html
+            historyBox.appendChild(historyCard)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+populateGenre();
 
 const popularSongBox = document.querySelector(".popular-song-box")
 const populateSong = async () => {
@@ -81,5 +109,6 @@ const populateSong = async () => {
         console.log(error);
     }
 }
+
 
 populateSong();
